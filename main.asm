59bd9ef7c2187b6f9bf83f7b26ea3c254e371644
Rafael Torres <rafaelsebastian0908@gmail.com>
2024-03-30 20:16:58 +0800

    TODO: fix snake length when eating fruit

--- main.asm
+++ main.asm
@@ -1,12 +1,11 @@
 .model small
 .stack 100h
-
 .data
     head_x dw 0Ah
     head_y dw 0Ah 
-    head_size dw 05h 
-    body_x dw 5 dup (?)
-    body_y dw 5 dup (?)
+    head_size dw 06h 
+    body_x dw 25 dup (?)
+    body_y dw 25 dup (?)
     body_size dw 0
     key_pressed db 'd'
     prev_key db ?
@@ -14,7 +13,7 @@
     food_x dw 05h
     food_y dw 04h
     
-    strScore dw 'Score:'
+    strScore db 'Score:'
     strScore_s equ $-strScore 
 
 .code
@@ -27,11 +26,15 @@
         mov ah, 0Bh
         mov bx, 0000h
         int 10h
+        mov si, offset body_x
+        mov di, offset body_y 
+        mov word ptr [ds:si], 0Ah
+        mov word ptr [ds:di], 0Ah
         call rng 
         game_loop:
-            call write_score
             call input
             call draw
+            call write_score
             call move
             call cls
             jmp game_loop
@@ -41,14 +44,40 @@
         mov ax, 1300h ; interrupt for write string
         mov bx, 000Fh ; set page number and color of string
    
-        mov dh, 02 ; row
-        mov dl, 22 ; col
-        sub dl, strScore_s ; offset 
+        mov dh, 0 ; row
+        mov dl, 0 ; col
         mov cx, strScore_s ; size of string
-        mov bp, offset game ; string in es:bp 
+        mov bp, offset strScore ; string in es:bp 
         int 10h
 
+        mov ah, 02h         ; place cursor beside score string
+        mov dl, 7
+        int 10h
+        
+        mov ax, body_size   ; convert score to decimal
+        aaa                 ; ah = tenths  |  al = ones
+        mov dx, ax          
+        add dh, '0'         ; convert to ascii
+        add dl, '0'
+        push dx
+
+        mov ah, 09h         
+        mov al, dh
+        mov bx, 000Fh
+        mov cx, 1
+        int 10h             ; write tenths place
+
+        mov ah, 02h         ; place cursor beside tenths place
+        mov dh, 0           
+        mov dl, 8
+        int 10h
+
+        pop dx
+        mov ah, 09h
+        mov al, dl
+        int 10h             ; write ones place 
         ret
+
     cls: ; clears the screen
         mov ah, 07h          ; Scroll up function
         mov al, 0            ; Number of lines to scroll
@@ -61,32 +90,31 @@
         mov cx, head_x
         mov dx, head_y
         
-        draw_head:
-            mov ax, 0c0fh
-            mov bh, 00h
-            int 10h             ; draw pixel
+        ;draw_head:
+        ;    mov ax, 0c0fh
+        ;    mov bh, 00h
+        ;    int 10h             ; draw pixel
 
-            inc cx              
-            mov ax, cx
-            sub ax, head_x 
-            cmp ax, head_size
-            jle draw_head       ; check x axis
+        ;    inc cx              
+        ;    mov ax, cx
+        ;    sub ax, head_x 
+        ;    cmp ax, head_size
+        ;    jle draw_head       ; check x axis
 
-            mov cx, head_x
-            inc dx 
+        ;    mov cx, head_x
+        ;    inc dx 
 
-            mov ax, dx
-            sub ax, head_y
-            cmp ax, head_size 
-            jle draw_head       ; check y axis
-        
+        ;    mov ax, dx
+        ;    sub ax, head_y
+        ;    cmp ax, head_size 
+        ;    jle draw_head       ; check y axis
         
         mov si, offset body_x
         mov di, offset body_y 
         mov bp, 0
         
-        cmp body_size, 0  ; only draw body if body_size > 0
-        je food
+        ;cmp body_size, 0  ; only draw body if body_size > 0
+        ;je food
 
         body: 
             mov cx, [ds:si+bp]
@@ -109,9 +137,13 @@
             sub ax, [ds:di+bp]
             cmp ax, head_size 
             jle draw_body    
+
             add bp, 2
-            cmp bp, body_size
-            jl body
+            mov ax, body_size
+            mov bx, 2
+            mul bx 
+            cmp bp, ax
+            jle body
 
         food:
             mov cx, food_x
@@ -178,19 +210,23 @@
 
         mov si, offset body_x 
         mov di, offset body_y
-        mov cx, head_x
-        mov dx, head_y
-        mov [ds:si], cx
-        mov [ds:di], dx
+
+        ;mov cx, head_x
+        ;mov dx, head_y
+        ;mov [ds:si], cx
+        ;mov [ds:di], dx
         mov bp, 2
         body_move: ; while moving, transfer the value of the head to the body
             mov ax, [ds:si+bp-2]
-            mov [ds:si+bp], ax
+            mov [ds:si+bp], ax             ;0 
             mov ax, [ds:di+bp-2]
             mov [ds:di+bp], ax
             add bp, 2
-            cmp bp, body_size
-            jle body_move
+            mov ax, body_size
+            mov bx, 2
+            mul bx
+            cmp bp, ax
+            jl body_move
         
         mov ax, head_size
         cmp key_pressed, 'w'
@@ -205,30 +241,30 @@
         move_up:
             cmp prev_key, 's'
             je ignore
-            sub head_y, ax
+            sub word ptr [ds:di], ax 
             jmp collision 
         move_down: 
             cmp prev_key, 'w'
             je ignore
-            add head_y, ax
+            add word ptr [ds:di], ax
             jmp collision 
         move_left: 
             cmp prev_key, 'd'
             je ignore
-            sub head_x, ax
+            sub word ptr [ds:si], ax
             jmp collision 
         move_right: 
             cmp prev_key, 'a'
             je ignore
-            add head_x, ax 
+            add word ptr [ds:si], ax 
             jmp collision
         ignore:
             mov ah, prev_key
             mov key_pressed, ah
         
         collision:      ; checks for collision with fruit
-            mov cx, head_x
-            mov dx, head_y
+            mov cx, word ptr [ds:si]
+            mov dx, word ptr [ds:di]
             mov ah, 0dh
             mov bh, 00h
             int 10h 
