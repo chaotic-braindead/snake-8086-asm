b069cfb3027547c23c1b60900e09ced4ec0964c7
Rafael Torres <rafaelsebastian0908@gmail.com>
2024-04-19 22:44:27 +0800

    complete overhaul of draw functions

--- main.asm
+++ main.asm
@@ -8,36 +8,32 @@
 ;       - food "rng" is not really random; sometimes food spawns on on snake's body 
 .model small
 .stack 10h
+
 .data
-    square_size dw 8h      ; change size of snake/food here
-    body_x dw 255 dup (?)    ; change max length of snake here
-    body_y dw 255 dup (?)    ; change max length of snake here
+    snake_pos dw 255 dup (?) ;;
     snake_length dw 0
     key_pressed db 'd'
     prev_key db ?
     time_now db 00h
-    food_x dw 10            
-    food_y dw 10
-    temp_x dw ?             
-    temp_y dw ?             
-    
-    strScore db 'Score:'
-    strScore_s equ $-strScore 
+    food_pos dw 0A0Ah
+    temp_pos dw ?
 
+   
+    strScore db 'Score:'
+    strScore_s equ $-strScore
+  
 .code
     mov ax, @data
     mov ds, ax 
-    mov es, ax 
     main:
         mov ax, 0013h
         int 10h
         mov ah, 0Bh
         mov bx, 0000h
         int 10h
-        lea si, body_x
-        lea di, body_y 
-        mov word ptr [si], 0Ah
-        mov word ptr [di], 0Ah
+        lea si, snake_pos ;;
+        mov word ptr [si], 0101h ;;   
+
         call rng 
         game_loop:
             call input
@@ -51,6 +47,8 @@
         mov ah, 4ch
         int 21h
     write_score:
+        mov ax, @data
+        mov es, ax 
         mov ax, 1300h ; interrupt for write string
         mov bx, 000Fh ; set page number and color of string
    
@@ -101,63 +99,166 @@
         ret
 
     draw:
-        lea si, body_x
-        lea di, body_y 
+        mov ax, @data
+        mov ds, ax 
+        lea si, snake_pos;;
+        mov dx, word ptr [si]
+        
+        mov ax, 0A000h ;; 
+        mov es, ax     ;;
+        mov ax, @code
+        mov ds, ax
+        push dx
+            mov ax, 8
+            mul dh
+            mov di, ax
+            mov ax, 8*320
+            mov bx, 0
+            add bl, dl
+            mul bx 
+            add di, ax
+        pop dx 
+
+        cmp key_pressed, 'w'
+        je head_up
+        cmp key_pressed, 's'
+        je head_down
+        cmp key_pressed, 'a'
+        je head_left
+        cmp key_pressed, 'd'
+        je head_right
+
+        head_up: 
+            lea si, snake_head_up
+            jmp draw_head
+        head_down: 
+            lea si, snake_head_down
+            jmp draw_head
+        head_left: 
+            lea si, snake_head_left
+            jmp draw_head
+        head_right: 
+            lea si, snake_head_right
+        
+        draw_head:
+            mov cl, 8
+        y_axis:
+            push di
+                mov ch, 8
+        x_axis:
+            mov al, [ds:si]
+            xor al, [es:di]
+            mov [es:di], al
+            inc si
+            inc di
+            dec ch
+            jnz x_axis
+        pop di
+        add di, 320
+        inc bl
+        dec cl 
+        jnz y_axis
+
         mov bp, 0
+        lea si, snake_pos;;
+        push si
         
-        body:
-            mov cx, word ptr [si] ; get snake head x coord 
-            mov dx, word ptr [di] ; get snake head y coord
-
-        draw_body:
-            mov ax, 0c0fh
-            mov bh, 00h
-            int 10h             ; draw pixel
-
-            inc cx              
-            mov ax, cx
-            sub ax, word ptr [si] 
-            cmp ax, square_size
-            jle draw_body       ; check x axis
-
-            mov cx, word ptr [si]
-            inc dx 
-
-            mov ax, dx          
-            sub ax, word ptr [di]
-            cmp ax, square_size 
-            jle draw_body       ; check y axis
-            
-            add si, 2   ; get next x and y coords
-            add di, 2
-            ; the next remaining lines are for checking if we have iterated through the entirety of the snake
-            inc bp         
+        try:
+            pop si
             cmp bp, snake_length
-            jle body
+            jge draw_food
+            add si, 2
+            mov ax, @data
+            mov ds, ax
+            mov dx, word ptr [si]
+            push si
 
-        mov cx, food_x
-        mov dx, food_y    
-        draw_food:
-            mov ax, 0c04h
-            mov bh, 00h
-            int 10h   
+            mov ax, 0A000h ;; 
+            mov es, ax     ;;
+            mov ax, @code
+            mov ds, ax
+            push dx
+                mov ax, 8
+                mul dh
+                mov di, ax
+                mov ax, 8*320
+                mov bx, 0
+                add bl, dl
+                mul bx 
+                add di, ax
+            pop dx 
 
-            inc cx 
-            mov ax, cx
-            sub ax, food_x 
-            cmp ax, square_size 
-            jle draw_food 
+            lea si, snake_body 
+            mov cl, 8
             
-            mov cx, food_x
-            inc dx 
-            mov ax, dx 
-            sub ax, food_y
-            cmp ax, square_size 
-            jle draw_food 
-        
-        ret 
+            draw_body_y:
+                push di 
+                mov ch, 8
+            draw_body_x:
+                mov al, [ds:si]
+                xor al, [es:di]
+                mov [es:di], al
+                inc si
+                inc di 
+                dec ch
+                jnz draw_body_x
+            pop di
+            add di, 320
+            inc bl
+            dec cl 
+            jnz draw_body_y 
+            inc bp
+            jmp try
+
+    draw_food: 
+    mov dx, food_pos
+    mov ax, 0A000h ;; 
+    mov es, ax     ;;
+    mov ax, @code
+    mov ds, ax
+    push dx
+        mov ax, 8
+        mul dh
+        mov di, ax
+        mov ax, 8*320
+        mov bx, 0
+        add bl, dl
+        mul bx 
+        add di, ax
+    pop dx 
+    lea si, food_map
+    mov cl, 8
+
+    food_y_axis:
+        push di
+            mov ch, 8
+    food_x_axis:
+        mov al, [ds:si]
+        xor al, [es:di]
+        mov [es:di], al
+        inc si
+        inc di
+        dec ch
+        jnz food_x_axis
+    pop di
+    add di, 320
+    inc bl
+    dec cl 
+    jnz food_y_axis
+  
+    break: ret 
     
     input:
+        mov ah, 02h
+        mov bx, 0
+        mov dx, 0502h
+        int 10h 
+        mov ah, 09h
+        mov al, key_pressed
+        mov bx, 000Fh
+        mov cx, 1
+        int 10h
+
         mov ah, 01h ; get user input
         int 16h
     
@@ -165,6 +266,7 @@
 
         mov ah, 00h
         int 16h
+        
         
         cmp al, 27 ; check if escape key
         jne update ; update key_pressed if not esc
@@ -175,20 +277,16 @@
             mov ah, key_pressed
             mov prev_key, ah
             mov key_pressed, al
+       
            
     back: ret 
 
     ; thank you stackoverflow, very cool https://stackoverflow.com/questions/40698309/8086-random-number-generator-not-just-using-the-system-time
     rng:       
         mov ax, 25173
-        mul word ptr food_x
+        mul food_pos
         add ax, 13849
-        mov food_x, ax
-
-        mov ax, 25173
-        mul word ptr food_y
-        add ax, 13849
-        mov food_y, bx
+        mov food_pos, ax
         ret
     
     move:
@@ -198,38 +296,27 @@
         je move     ; keep checking until we have a different time
         mov time_now, dl
 
-        lea si, body_x 
-        lea di, body_y
-
-        mov bp, 1
-        mov cx, word ptr [si]
-        mov temp_x, cx              ; temporarily store the value of snake head x coord
-        mov cx, word ptr [di]    
-        mov temp_y, cx              ; temporarily store the value of snake head y coord
-        
+        mov ax, @data
+        mov ds, ax
+        lea si, snake_pos
+        mov bp, 0
+        mov dx, word ptr [si]
+        mov temp_pos, dx
         body_move: 
             add si, 2                       ; get next x and y coords
-            add di, 2
-            mov cx, temp_x                  ; get previous x coord   
-            mov dx, word ptr [si]
-            mov word ptr [si], cx
-            mov temp_x, dx                  ; store current x coord value for next iteration
-            
-            mov cx, temp_y                  ; get previous y coord 
-            mov dx, word ptr [di]
-            mov word ptr [di], cx
-            mov temp_y, dx                  ; store current y coord value for next iteration
-            
-           
-            ; next remaining lines are for checking if we have iterated through the entirety of the snake
-            inc bp                           
+            mov dx, word ptr [si]  
+            mov cx, temp_pos
+            mov word ptr [si], cx 
+            mov temp_pos, dx
+            inc bp 
             cmp bp, snake_length
-            jle body_move
+            jl body_move
         
-        lea si, body_x
-        lea di, body_y
+        lea si, snake_pos
+        mov ax, @data
+        mov ds, ax
         check_key:
-            mov ax, square_size
+            mov dx, word ptr [si]
             cmp key_pressed, 'w'
             je move_up 
             cmp key_pressed, 'a' 
@@ -242,41 +329,118 @@
         move_up:
             cmp prev_key, 's'       ; if the previous key is the opposite direction, do nothing
             je ignore
-            sub word ptr [di], ax 
-            jmp collision 
+            dec dl 
+            push dx
+            call collision
+            pop dx
+            mov word ptr [si], dx
+            jmp return
         move_down: 
             cmp prev_key, 'w'
             je ignore
-            add word ptr [di], ax
-            jmp collision 
+            inc dl 
+            push dx
+            call collision
+            pop dx
+            mov word ptr [si], dx
+            ;mov word ptr [si], dx
+            ;pop di
+            jmp return 
         move_left: 
             cmp prev_key, 'd'
             je ignore
-            sub word ptr [si], ax
-            jmp collision 
+            dec dh 
+            push dx
+            call collision
+            pop dx
+            mov word ptr [si], dx
+            jmp return 
         move_right: 
             cmp prev_key, 'a'
             je ignore
-            add word ptr [si], ax 
-            jmp collision
+            inc dh
+            push dx 
+            call collision
+            pop dx 
+            mov word ptr [si], dx
+            jmp return 
         ignore: 
             mov ah, prev_key
             mov key_pressed, ah
             jmp check_key
-        
-        collision:      ; checks for collision with food   ! TODO: collision with self !
-            mov cx, word ptr [si]
-            mov dx, word ptr [di]
-            mov ah, 0dh
-            mov bh, 00h
-            int 10h 
-       ; insert self collision here
+       
+    return: 
+        ret
 
-       ; food collision
-            cmp al, 04h
-            jne return 
-            inc snake_length
-            call rng 
+ collision:
+    mov ah, 0dh
+    mov bx, 0
+    mov cx, dx 
+    and cx, 0FF00h
+    mov cl, ch
+    xor ch, ch 
+    and dx, 00FFh
+    int 10h
+    cmp al, 0Ch
+    jne wee 
+    inc snake_length
+    call rng     
+    wee:
+        ret
             
-        return: ret
+    snake_head_up: 
+        DB 00h,00h,00h,00h,00h,00h,00h,00h     ;  0
+        DB 00h,00h,00h,0Ch,0Ch,00h,00h,00h     ;  1
+        DB 00h,00h,0Ah,0Ah,0Ah,0Ah,00h,00h     ;  2
+        DB 00h,0Ah,00h,0Ah,0Ah,00h,0Ah,00h     ;  3
+        DB 00h,0Ah,0Ah,0Ah,0Ah,0Ah,0Ah,00h     ;  4
+        DB 00h,02h,0Ah,0Ah,0Ah,0Ah,02h,00h     ;  5
+        DB 00h,02h,0Ah,0Ah,0Ah,0Ah,02h,00h     ;  6
+        DB 00h,00h,02h,0Ah,0Ah,02h,00h,00h     ;  7
+    snake_head_down: 
+        DB 00h,00h,02h,0Ah,0Ah,02h,00h,00h     ;  7
+        DB 00h,02h,0Ah,0Ah,0Ah,0Ah,02h,00h     ;  6
+        DB 00h,02h,0Ah,0Ah,0Ah,0Ah,02h,00h     ;  5
+        DB 00h,0Ah,0Ah,0Ah,0Ah,0Ah,0Ah,00h     ;  4
+        DB 00h,0Ah,00h,0Ah,0Ah,00h,0Ah,00h     ;  3
+        DB 00h,00h,0Ah,0Ah,0Ah,0Ah,00h,00h     ;  2
+        DB 00h,00h,00h,0Ch,0Ch,00h,00h,00h     ;  1
+        DB 00h,00h,00h,00h,00h,00h,00h,00h     ;  0
+    snake_head_left: 
+        DB 00h,00h,00h,00h,00h,00h,00h,00h     ;  0
+        DB 00h,00h,02h,0Ah,0Ah,02h,00h,00h     ;  7
+        DB 00h,00h,00h,0Ch,0Ch,00h,00h,00h     ;  1
+        DB 00h,0Ah,00h,0Ah,0Ah,00h,0Ah,00h     ;  3
+        DB 00h,00h,0Ah,0Ah,0Ah,0Ah,00h,00h     ;  2
+        DB 00h,02h,0Ah,0Ah,0Ah,0Ah,02h,00h     ;  6
+        DB 00h,0Ah,0Ah,0Ah,0Ah,0Ah,0Ah,00h     ;  4
+        DB 00h,02h,0Ah,0Ah,0Ah,0Ah,02h,00h     ;  5
+    snake_head_right: 
+        DB 00h,0Ah,0Ah,0Ah,0Ah,0Ah,0Ah,00h     ;  4
+        DB 00h,00h,00h,0Ch,0Ch,00h,00h,00h     ;  1
+        DB 00h,00h,00h,00h,00h,00h,00h,00h     ;  0
+        DB 00h,00h,0Ah,0Ah,0Ah,0Ah,00h,00h     ;  2
+        DB 00h,0Ah,00h,0Ah,0Ah,00h,0Ah,00h     ;  3
+        DB 00h,00h,02h,0Ah,0Ah,02h,00h,00h     ;  7
+        DB 00h,02h,0Ah,0Ah,0Ah,0Ah,02h,00h     ;  6
+        DB 00h,02h,0Ah,0Ah,0Ah,0Ah,02h,00h     ;  5
+    snake_body:
+        DB 00h,00h,00h,00h,00h,00h,00h,00h     ;  0
+        DB 00h,02h,0Ah,0Ah,0Ah,0Ah,02h,00h     ;  1
+        DB 00h,0Ah,02h,0Ah,0Ah,02h,0Ah,00h     ;  2
+        DB 00h,0Ah,0Ah,0Ah,0Ah,0Ah,0Ah,00h     ;  3
+        DB 00h,0Ah,0Ah,0Ah,0Ah,0Ah,0Ah,00h     ;  4
+        DB 00h,0Ah,02h,0Ah,0Ah,02h,0Ah,00h     ;  5
+        DB 00h,02h,0Ah,0Ah,0Ah,0Ah,02h,00h     ;  6
+        DB 00h,02h,0Ah,0Ah,0Ah,0Ah,02h,00h     ;  7
+    food_map:
+        DB 00h,00h,00h,00h,0Ah,00h,00h,00h     ;  0
+        DB 00h,00h,00h,0Ah,02h,00h,00h,00h     ;  1
+        DB 00h,00h,0Ch,00h,00h,0Ch,00h,00h     ;  2
+        DB 00h,0Ch,0Fh,0Ch,0Ch,0Ch,0Ch,00h     ;  3
+        DB 00h,0Fh,0Ch,0Ch,0Ch,0Ch,0Ch,00h     ;  4
+        DB 00h,0Ch,0Ch,0Ch,0Ch,0Ch,04h,00h     ;  5
+        DB 00h,00h,0Ch,0Ch,04h,04h,00h,00h     ;  6
+        DB 00h,00h,00h,00h,00h,00h,00h,00h     ;  7
+    
 end
